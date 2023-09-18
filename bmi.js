dced32f72375dc0e3dfc0378e44f8a345df6e317
Victor Tyavbee <68505976+Iamtsquare07@users.noreply.github.com>
2023-09-18 19:39:18 +0100

    Add files via upload
    Updated code and added few changes

--- bmi.js
+++ bmi.js
@@ -5,30 +5,30 @@ const calculateButton = document.querySelector('#calculate-button-bmi-calculator
 const outputText = document.querySelector('#output-text-bmi-calculator');
 
 calculateButton.addEventListener('click', () => {
-  // Get the height and weight inputs
-  const height = Number(heightInput.value);
-  const weight = Number(weightInput.value);
+    // Get the height and weight inputs
+    const height = Number(heightInput.value);
+    const weight = Number(weightInput.value);
 
-  // Calculate the BMI
-  const bmi = weight / (height / 100) ** 2;
+    // Calculate the BMI
+    const bmi = weight / (height / 100) ** 2;
 
-  if(height > 1 && weight > 1) {
-    // Display the result
-    outputText.innerHTML = `Your BMI is ${bmi.toFixed(1)}.`;
+    if (height > 1 && weight > 1) {
+        // Display the result
+        outputText.innerHTML = `Your BMI is ${bmi.toFixed(1)}.`;
 
-      // Add additional information about the result
-      if (bmi < 18.5) {
-        outputText.innerHTML += ' You are underweight.';
-      } else if (bmi < 25) {
-        outputText.innerHTML += ' You are at a very healthy weight. Keep it up';
-      } else if (bmi < 30) {
-        outputText.innerHTML += ' You are overweight.';
-      } else {
-        outputText.innerHTML += ' You are obese.';
-      }
-  }else {
-    heightInput.focus()
-    outputText.innerHTML += ' Please enter a valid height and weight';
-  }
-  
+        // Add additional information about the result
+        if (bmi < 18.5) {
+            outputText.innerHTML += ' You are underweight.';
+        } else if (bmi < 25) {
+            outputText.innerHTML += ' You are at a very healthy weight. Keep it up';
+        } else if (bmi < 30) {
+            outputText.innerHTML += ' You are overweight.';
+        } else {
+            outputText.innerHTML += ' You are obese.';
+        }
+    } else {
+        heightInput.focus()
+        outputText.innerHTML += ' Please enter a valid height and weight';
+    }
+
 });
